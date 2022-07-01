2fe2ed5a1ab9876ea71e932c004ef854e2d7cfc4
Linh Bach <linh.bach@jmango360.com>
2022-07-01 13:35:26 +0700

    update factory

--- src/main/java/com/jmango/jamapi/factory/AppServiceFactory.java
+++ src/main/java/com/jmango/jamapi/factory/AppServiceFactory.java
@@ -4,22 +4,38 @@ import com.jmango.jamapi.facade.AppService;
 import com.jmango.jamapi.service.MagentoAppService;
 import com.jmango.jamapi.service.SeoShopAppService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @AllArgsConstructor
+@Slf4j
 public class AppServiceFactory {
     ApplicationContext context;
 
-    public AppService getAppService(String appKey) {
-        switch(appKey) {
-            case "magentoApp":
-                return context.getBean(MagentoAppService.class);
-            case "SEOShopApp":
-                return context.getBean(SeoShopAppService.class);
-            default:
-                throw new IllegalArgumentException("app is not supported");
+    private static Map<String, AppService> serviceMap = new HashMap<>();
+
+    public interface Injector extends AppService {
+        String getType();
+
+        @PostConstruct
+        default void inject() {
+            log.info("Inject App Service: " + getType());
+            serviceMap.put(getType(), this);
         }
+    }
+
+    public AppService getAppService(String appKey) {
+        AppService service = serviceMap.get(appKey);
+        if (service == null) {
+            throw new IllegalArgumentException("app is not supported");
+        }
+
+        return service;
     }
 }
--- src/main/java/com/jmango/jamapi/service/MagentoAppService.java
+++ src/main/java/com/jmango/jamapi/service/MagentoAppService.java
@@ -1,11 +1,19 @@
 package com.jmango.jamapi.service;
 
+import com.jmango.jamapi.constant.Const;
 import com.jmango.jamapi.entity.ApplicationDAO;
 import com.jmango.jamapi.facade.AppService;
+import com.jmango.jamapi.factory.AppServiceFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MagentoAppService implements AppService {
+public class MagentoAppService implements AppService, AppServiceFactory.Injector {
+
+    @Override
+    public String getType() {
+        return Const.AppService.MAGENTO_APP_KEY;
+    }
+
     @Override
     public ApplicationDAO createApp(String owner, String appName) {
         return new ApplicationDAO();
